7cdc2238d6ed68ef5fa1a70a80bd508b13a42798
Mark Meliksetyan <markmelix@gmail.com>
2025-11-22 14:17:59 +0300

    impl SetIsActiveUser usecase

--- internal/service/user.go
+++ internal/service/user.go
@@ -1,12 +1,19 @@
 package service
 
+import (
+	"context"
+	"fmt"
+)
+
 type User struct {
 	Id       string
 	Username string
 	IsActive bool
 }
 
-type UserRepo interface{}
+type UserRepo interface {
+	SetIsActiveUser(ctx context.Context, id string, isActive bool) error
+}
 
 type UserService struct {
 	Repo UserRepo
@@ -14,4 +21,11 @@ type UserService struct {
 
 func NewUserService(repo UserRepo) *UserService {
 	return &UserService{repo}
+}
+
+func (uc *UserService) SetIsActiveUser(ctx context.Context, id string, isActive bool) error {
+	if err := uc.Repo.SetIsActiveUser(ctx, id, isActive); err != nil {
+		return fmt.Errorf("failed toggling user: %w", err)
+	}
+	return nil
 }
