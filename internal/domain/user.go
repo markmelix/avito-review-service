9fe7c13d9ea52d4545b6671d4cc13192d63bb73f
Mark Meliksetyan <markmelix@gmail.com>
2025-11-23 14:03:48 +0300

    remove empty team_name return on /teams/add endpoint

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -4,5 +4,5 @@ type User struct {
 	Id       string `json:"user_id"`
 	Username string `json:"username"`
 	IsActive bool   `json:"is_active"`
-	TeamName string `json:"team_name"`
+	TeamName string `json:"team_name,omitempty"`
 }
